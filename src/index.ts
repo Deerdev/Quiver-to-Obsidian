fe83e05932c44aa35295044db08aa02b7aa826a6
DeerDev <daoquanlu@gmail.com>
2022-03-29 22:22:27 +0800

    feat: add -v to cli

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@
 import { Command } from 'commander';
 import chalk from 'chalk';
 import Quiver from './quiver/index.js';
+import * as fs from 'fs';
 
 const program = new Command();
 const { log } = console;
+const version = JSON.parse(fs.readFileSync(new URL('../package.json', import.meta.url)).toString()).version
 
 program.requiredOption('-q, --quiver-path <path>', 'quiver library dir path')
   .requiredOption('-o, --output-path <path>', 'output dir path')
   .option('-e, --ext-names [ext...]', '[option] replace some unknown resource image file ext to `png`');
 program.showHelpAfterError();
+program.version(version, '-v, --version')
 
 program.parse(process.argv);
 const options = program.opts();
